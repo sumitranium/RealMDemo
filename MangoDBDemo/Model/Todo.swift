e29a77d907746242a8d7a04d694844d5159a2ca1
Sumit-Ranium <ranium@Raniums-MacBook-Pro.local>
2023-02-02 17:48:10 +0530

    Initial commit

---
+++ MangoDBDemo/Model/Todo.swift
@@ -0,0 +1,20 @@
+//
+//  Todo.swift
+//  MangoDBDemo
+//
+//  Created by Sumit-Ranium on 01/02/23.
+//
+
+import Foundation
+
+//class Furniture: AnyObject {
+//    @Persisted(primaryKey: true) var _id: ObjectId
+//    @Persisted var name: String = ""
+//    @Persisted var status: String = ""
+//    @Persisted var ownerId: String = ""
+//    convenience init(name: String, ownerId: String) {
+//        self.init()
+//        self.name = name
+//        self.ownerId = ownerId
+//    }
+//}
